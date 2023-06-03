2a305bc1f48275bc03b5e5d31af82b7a291b7b7a
Caio <kayoroxa@gmail.com>
2023-06-03 16:38:35 -0300

    fix parse pattern

--- src/utils/strToPattern.ts
+++ src/utils/strToPattern.ts
@@ -1,20 +1,24 @@
 function transform(stringPattern) {
-  stringPattern = stringPattern.trim()
-  const rawSentence = stringPattern.split(/\n\n+/g)[0]
-  const groups = stringPattern.split(/\n\n+/g).slice(1)
+  try {
+    stringPattern = stringPattern.trim()
+    const rawSentence = stringPattern.split(/\n\n+/g)[0]
+    const groups = stringPattern.split(/\n\n+/g).slice(1)
 
-  const replacements = groups.map(group => {
-    const [id, ...alternatives] = group.split(/\n/g).filter(Boolean)
+    const replacements = groups.map(group => {
+      const [id, ...alternatives] = group.split(/\n/g).filter(Boolean)
+
+      return {
+        id: id?.replace(/\:/, ''),
+        alternatives,
+      }
+    })
 
     return {
-      id: id?.replace(/\:/, ''),
-      alternatives,
+      rawSentence,
+      replacements,
     }
-  })
-
-  return {
-    rawSentence,
-    replacements,
+  } catch (error) {
+    return false
   }
 }
 
@@ -34,7 +38,9 @@ export function strToPattern(allPatternsStr: string): Type[] | false {
 
   const bigData = allPatternsStr
     .split(/[*-\/\\]{2,}/g)
+    .filter(v => v.trim().length > 0)
     .map(strPattern => transform(strPattern))
+    .filter(v => v !== false) as Type[]
 
   return bigData
 }
